79bc086239a5ff67ddeacb84ada39f2aca153df5
NitroRiser <jonathanfariastech@gmail.com>
2022-08-18 12:22:15 +0000

    map exercise

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
+
+import entities.Product;
 
 /**
  * Main
@@ -10,25 +12,21 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Map<String, String> cookies = new TreeMap<>();
+        Map<Product, Double> stock = new HashMap<>();
 
-        cookies.put("username", "Maria");
-        cookies.put("email", "maria@gmail.com");
-        cookies.put("phone", "99711122");
+        Product p1 = new Product("Tv", 900.00);
+        Product p2 = new Product("Notebook", 1200.00);
+        Product p3 = new Product("Tablet ", 400.00);
 
-        cookies.remove("email");
-        cookies.put("phone", "997711033");
-        System.out.println("Phone number: " + cookies.get("phone"));
-        System.out.println("Email: " + cookies.get("email"));
-        System.out.println("Size: " + cookies.size());
+        stock.put(p1, 10000.0);
+        stock.put(p2, 20000.0);
+        stock.put(p3, 15000.0);
+
+        Product ps = new Product("Tv", 900.00);
 
 
-        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+        System.out.println();
+        System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
 
-        System.out.println("ALL COOKIES:");
-
-        for (String key : cookies.keySet()){
-            System.out.println(key + ": " + cookies.get(key));
-        }
     }
 }
